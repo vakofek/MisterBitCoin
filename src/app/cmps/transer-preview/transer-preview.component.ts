e9e9c849f91df61656717bc1b785dac09271c25d
ofek vaknin <vakofek10@gmail.com>
2021-06-22 19:32:25 +0300

    more style

--- src/app/cmps/transer-preview/transer-preview.component.ts
+++ src/app/cmps/transer-preview/transer-preview.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { BitconService } from 'src/app/services/bitcoin.service';
 import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
@@ -10,14 +11,16 @@ import { ContactService } from 'src/app/services/contact.service';
 export class TranserPreviewComponent implements OnInit {
   @Input() move: { type: string, transferAt: number, userId?: string, contactId?: string, coins: number }
   @Input() title: string
-  
-  constructor(private contactService: ContactService) { }
+
+  constructor(private contactService: ContactService, private bitconService: BitconService) { }
 
   contactName: string
   subscription: Subscription
+  marketPrice: number
 
   ngOnInit(): void {
     this.subscription = this.contactService.getContactById(this.move.contactId).subscribe(contact => this.contactName = contact.name);
+    this.marketPrice = this.bitconService.getMarketPrice()[0].y;
   }
 
   ngOnDestroy() {
